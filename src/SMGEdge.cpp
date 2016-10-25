2621f0d4165b833d147c48ba0685daad6952289f
iwanao731 <iwanao731@gmail.com>
2016-10-25 10:58:14 -0700

    add SMGEdge

--- src/SMGEdge.cpp
+++ src/SMGEdge.cpp
@@ -7,3 +7,38 @@
 //
 
 #include "SMGEdge.h"
+
+SMGEdge::SMGEdge() : mError(0.0f)
+{
+    
+}
+
+SMGEdge::~SMGEdge()
+{
+    
+}
+
+void SMGEdge::setStartNode(SMGNode *node)
+{
+    this->mNode1 = node;
+}
+
+void SMGEdge::setDestNode(SMGNode *node)
+{
+    this->mNode2 = node;
+}
+
+void SMGEdge::setError(const float &errorValue)
+{
+    this->mError = errorValue;
+}
+
+SMGNode *SMGEdge::getStartNode() const
+{
+    return this->mNode1;
+}
+
+SMGNode *SMGEdge::getDestNode() const
+{
+    return this->mNode2;
+}
--- src/SMGEdge.h
+++ src/SMGEdge.h
@@ -10,5 +10,27 @@
 #define ____SMGEdge__
 
 #include <stdio.h>
+#include "SMGNode.h"
+
+class SMGNode;
+
+class SMGEdge
+{
+public:
+    SMGEdge();
+    ~SMGEdge();
+    
+    void setStartNode(SMGNode *node);
+    void setDestNode(SMGNode *node);
+    void setError(const float &errorValue);
+    
+    SMGNode *getStartNode() const;
+    SMGNode *getDestNode() const;
+    
+private:
+    float mError;
+    SMGNode *mNode1;
+    SMGNode *mNode2;
+};
 
 #endif /* defined(____SMGEdge__) */
